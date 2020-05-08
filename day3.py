fe2b588467bf0d0d540a68e7916f71262044c99a
Dmitry Legchikov <source>
2020-05-08 09:08:46 +0300

    day 3 second solution

--- day3.py
+++ day3.py
@@ -2,9 +2,14 @@
 
 # https://leetcode.com/problems/single-number/
 
-class Solution:
+class Solution1:
     def singleNumber(self, nums: List[int]) -> int:
         result = 0
         for n in nums:
             result ^= n
         return result
+
+
+class Solution2:
+    def singleNumber(self, nums: List[int]) -> int:
+        return 2*sum(set(nums)) - sum(nums)
