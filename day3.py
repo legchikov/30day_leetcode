510c91d5d91dc5379b5f99f53abba06965b4807f
Dmitry Legchikov <source>
2020-05-08 09:14:20 +0300

    day3 update

--- day3.py
+++ day3.py
@@ -1,6 +1,8 @@
 # 136. Single Number
-
 # https://leetcode.com/problems/single-number/
+
+from typing import List
+
 
 class Solution1:
     def singleNumber(self, nums: List[int]) -> int:
@@ -13,3 +15,19 @@ class Solution1:
 class Solution2:
     def singleNumber(self, nums: List[int]) -> int:
         return 2*sum(set(nums)) - sum(nums)
+        
+
+if __name__ == '__main__':
+    s1 = Solution1()
+    result = s1.singleNumber([2, 2, 1])
+    assert result == 1
+    result = s1.singleNumber([4, 4, 2, 1, 1])
+    assert result == 2
+    print('OK')
+
+    s2 = Solution2()
+    result = s2.singleNumber([2, 2, 1])
+    assert result == 1
+    result = s2.singleNumber([4, 4, 2, 1, 1])
+    assert result == 2
+    print('OK')
